9b9ed373c9ecf5c106443e28fe202970ae375ff5
Eduardo Simón Picón <al341513@uji.es>
2020-11-28 10:02:32 +0100

    fix lint issues

--- src/modules/search/search-page.js
+++ src/modules/search/search-page.js
@@ -1,5 +1,7 @@
 import {useRouteMatch} from 'react-router-dom';
+import React from 'react';
 
+// eslint-disable-next-line require-jsdoc
 export default function SearchPage() {
   const match = useRouteMatch('hello');
 

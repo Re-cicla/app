031aebb2332fc8d1f1906916cc0445f298762138
Eduardo Simón Picón <al341513@uji.es>
2020-11-28 10:22:32 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={SearchPage} />
+        <Route path="/" component={SearchPage} />
       </Switch>
     </BrowserRouter>
   );
